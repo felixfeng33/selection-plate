5cdbb09f8b9c8b5fce0b63dc35a259dc5d7e39e0
Felix Feng <felixfeng33@gmail.com>
2024-08-21 21:15:02 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,87 @@
+"use client"
 import Image from "next/image"
+import SelectionArea from ".."
+import React, { useEffect } from "react"
+
+let called = false
 
 export default function Home() {
+  useEffect(()=>{
+    if(called) return
+    called = true
+    const selection = new SelectionArea({
+      selectables: ["main > section > li"],
+      boundaries: ["main > section"],
+      container:"#scroll_container",
+      selectionContainerClass:"clipping-container",
+    })
+      .on("start", ({ store, event }) => {
+        if (!(event as MouseEvent).ctrlKey && !(event as MouseEvent).metaKey) {
+          for (const el of store.stored) {
+            el.classList.remove("selected")
+          }
+  
+          selection.clearSelection()
+        }
+      })
+      .on(
+        "move",
+        ({
+          store: {
+            changed: { added, removed },
+          },
+        }) => {
+          for (const el of added) {
+            el.classList.add("selected")
+          }
+  
+          for (const el of removed) {
+            el.classList.remove("selected")
+          }
+        }
+      )
+  },[])
+
+  /* eslint-disable no-console */
+  // selection
+  //   .on("beforestart", (evt) => console.log("beforestart", evt))
+  //   .on("start", (evt) => console.log("start", evt))
+  //   .on("beforedrag", (evt) => console.log("beforedrag", evt))
+  //   .on("move", (evt) => console.log("move", evt))
+  //   .on("stop", (evt) => console.log("stop", evt))
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <section className="w-[900px] p-20 h-[500px] bg-gray-200 overflow-y-auto">
-        <div  className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
-        <div className="text-center">item</div>
+      <section id="scroll_container" className="relative w-[900px] p-20 h-[500px] bg-gray-200 overflow-y-auto [&_li]:text-center [&_li]:select-none">
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
       </section>
     </main>
   )
